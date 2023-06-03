7abe90a4ea9f7aa04bca12130b029ee6ebd03bb7
Claudio R <claudior2184@gmail.com>
2023-06-02 20:44:15 -0600

    Docker compose configs for Discount API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,5 +28,6 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+app.MigrateDatabase<Program>();
 
 app.Run();
